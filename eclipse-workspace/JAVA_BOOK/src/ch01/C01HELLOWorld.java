67cb63c8db1725b0685d42201d5b36ecbf4c4a81
baekboowoo <131091538+baekboowoo@users.noreply.github.com>
2023-05-23 15:37:32 +0900

    Add files via upload

---
+++ eclipse-workspace/JAVA_BOOK/src/ch01/C01HELLOWorld.java
@@ -0,0 +1,22 @@
+package ch01; // 패키지명(폴더와 유사)
+
+// method : 방법,방식....
+// 함수 : 데이터를 담에 처리하는 논리적 공간
+
+public class C01HELLOWorld //클래스 영역 - 객체지향 문법 적용 단위
+{
+	
+	// 메서드 : 작업수행을 한는데 필요한 여러 코드를 묶어서 실행하는 단위
+	// main 메서드 : 어떤 프로그램을 실행하는데 최초의 실행 함수
+	// library 메서드 : 미리 만들어져 제공되어지는 메서드
+	// 사용자 정의 메서드 : 개발자에 의해 만들어지는 메서드
+
+	public static int main(String[] args) //메서드 영역 - 절차 지향 문법 적용 단위
+	{
+			
+			System.out.println("HELLO World");
+			return 1;
+			
+	}
+
+}
